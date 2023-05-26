63ab41eadc8a1c5aa3e865ba9f3a27918bd6b92f
Artem <artchzhen@gmail.com>
2023-05-26 18:31:47 +0900

    initial commit development

---
+++ project28/project28/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project28
+//
+//  Created by Артем Чжен on 26/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
